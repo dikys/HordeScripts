6dcb1514f1310c5735457e23a028bc825059454c
Horde Resurrection <65279361+HordeResurrection@users.noreply.github.com>
2024-02-16 21:49:56 +0300

    fix getUnitProfessionParams

--- Scripts/library/game-logic/unit-professions.ts
+++ Scripts/library/game-logic/unit-professions.ts
@@ -11,13 +11,12 @@ var UnitProducerProfessionData = HCL.HordeClassLibrary.UnitComponents.Profession
  * Берет параметры профессии из конфига юнита.
  * 
  * Почему-то не получилось просто взять значение по ключу из словаря (см. ниже)
- * В целом этот метод тоже пока что не работает как надо, т.к. объект с итоговыми параметрами имеет не тот тип.
- * Оставил, как пример работы с out-параметром.
+ * Пришлось переделать через out-параметр.
  * 
  * @param uCfg - конфиг юнита
  * @param prof - профессия (значение из enum UnitProfession) (TODO: можно сделать парсинг из строки)
  * 
- * @return result - the concatenated description of all object properties
+ * @return result - параметры профессии
  */
 function getUnitProfessionParams(uCfg, prof) {
     var profParams = host.newVar(HCL.HordeClassLibrary.HordeContent.Configs.Units.ProfessionParams.AUnitProfessionParams);
@@ -25,7 +24,7 @@ function getUnitProfessionParams(uCfg, prof) {
         logw('Can\'t get profession params:', prof.ToString());
         return null;
     }
-    return profParams;
+    return profParams.value;
 
     // Так почему-то не вышло:
     // var prof = uCfg.ProfessionParams.Item.get(prof);
