f6c6fa948c0f87b49442a10cfad1cad4c4629bdd
night-ghost-1 <nightgho5t@yandex.ru>
2024-03-10 15:28:01 +0300

    Add more variety to settlement development

--- Scripts/Mira/SettlementControllerStates/04_DevelopingState.ts
+++ Scripts/Mira/SettlementControllerStates/04_DevelopingState.ts
@@ -1,6 +1,6 @@
 
 class DevelopingState extends ProductionState {
-    protected readonly MAX_PRODUCTION_TIME = 6000; //2 min
+    protected readonly MAX_PRODUCTION_TIME = 6000 / 2; //2 min
     
     protected getTargetUnitsComposition(): UnitComposition {
         var targetCompostion = new Map<string, number>();
@@ -10,26 +10,44 @@ class DevelopingState extends ProductionState {
 
         let economyComposition = this.settlementController.GetCurrentEconomyComposition();
         let produceableCfgIds = this.settlementController.ProductionController.GetProduceableCfgIds();
-
-        let expansionRequested = false;
+        let absentProducers = [];
+        let absentTech = [];
 
         for (let cfgId of produceableCfgIds) {
             if (economyComposition.has(cfgId)) {
                 continue;
             }
 
-            if (
-                !expansionRequested && (
-                    MiraUtils.IsProducerConfig(cfgId) ||
-                    MiraUtils.IsTechConfig(cfgId)
-                )
-            ) {
-                expansionRequested = true;
+            if (MiraUtils.IsProducerConfig(cfgId)) {
+                absentProducers.push(cfgId);
+            }
+            else if (MiraUtils.IsTechConfig(cfgId)) {
+                absentTech.push(cfgId);
+            }
+        }
 
-                if (!targetCompostion.has(cfgId)) {
-                    targetCompostion.set(cfgId, 1);
+        if (absentProducers.length > 0 || absentTech.length > 0) {
+            let selectedCfgIds: Array<string>;
+
+            if (absentProducers.length > 0 && absentTech.length > 0) {
+                let pick = MiraUtils.Random(100, 1);
+                
+                if (pick > 70) {
+                    selectedCfgIds = absentTech;
+                }
+                else {
+                    selectedCfgIds = absentProducers;
                 }
             }
+            else if (absentProducers.length > 0) {
+                selectedCfgIds = absentProducers;
+            }
+            else {
+                selectedCfgIds = absentTech;
+            }
+            
+            let index = MiraUtils.Random(selectedCfgIds.length - 1);
+            MiraUtils.IncrementMapItem(targetCompostion, selectedCfgIds[index]);
         }
 
         let combatComposition = this.settlementController.StrategyController.GetArmyComposition();
