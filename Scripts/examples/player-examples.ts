ed70913eab6de2e956a715a1c05cd2ebd5b6f84e
Horde Resurrection <65279361+HordeResurrection@users.noreply.github.com>
2024-03-19 01:19:35 +0300

    fix comment

--- Scripts/examples/player-examples.ts
+++ Scripts/examples/player-examples.ts
@@ -58,7 +58,7 @@ export class Example_PlayerWorks extends HordeExampleBase {
             } else {
                 this.log.info('  Невозможно определить источник ввода игрока:', realPlayer);
             }
-            // Дополнительно ещё есть "realPlayer.IsNotLocal" - означает либо реплей-игрока, либо удаленного игрока
+            // Дополнительно ещё есть "realPlayer.IsNotLocal" - означает либо replay-игрока, либо remote-игрока
 
             this.log.info('- Источник управления:');
             if (realPlayer.IsBot) {
