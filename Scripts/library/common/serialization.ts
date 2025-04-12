b009baf76dbedd9cbf16506259864d410fea1f04
dikys <lrdikysa@mail.ru>
2025-04-12 20:05:48 +0500

    реструктуризация
    - переработал структуру репозитория

--- Scripts/library/common/serialization.ts
+++
@@ -1,8 +0,0 @@
-
-/**
- * Это обычный js-сериализатор. Он редко дружит с .Net-объектами.
- * TODO: Проверить отличия при работе с флагом "EnableStringifyEnhancements"
- */
-export function toJsonMy(object: any) {
-    return JSON.stringify(object, null, 2);
-}
